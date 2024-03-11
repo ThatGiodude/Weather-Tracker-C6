e68ba70479f29896c55931783d1196c5de472ebf
ThatGiodude <giovannifstrangio@gmail.com>
2024-03-11 18:58:56 -0400

    added functions for retrieving data and displaying it. fixed html tag

--- Assets/script.js
+++ Assets/script.js
@@ -7,3 +7,25 @@ const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", getInput)
+
+function getInput (event) {
+    event.preventDefault();
+    if (event.type === "click") {
+        getData(search.value)
+    }
+}
+
+function getData(city) {
+    fetch(`${api.base}?q=${city}&units=metrics&appid=${api.key}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(displayData)
+}
+
+function displayData( response) {
+    if (response.cod === "404") {
+        const error = document.querySelector(.error)
+        error.textContent = "Please Enter a Valid City Name"
+    }
+}
