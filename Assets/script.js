d9fc355c14a337272751b2bd1acb3640a730622d
ThatGiodude <giovannifstrangio@gmail.com>
2024-03-12 14:05:36 -0400

    set up basic function for pulling the city information

--- Assets/script.js
+++ Assets/script.js
@@ -30,3 +30,39 @@ function getWeather() {
         alert('Error fetching daily Forecast Data. Please try again.');
     });
 }
+
+function displayWeather(data) {
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+    const dailyForecastDiv = document.getElementById('daily-forecast');
+    // clears the content in the field
+    weatherInfoDiv.innerHTML = '';
+    dailyForecastDiv.innerHTML = '';
+    tempDivInfo.innerHTML ='';
+
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconURL = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        const temperatureHTML = `
+        <p>${temperature}0C</p>
+        `;
+        const weatherHTML = `
+        <p>${cityName}</p>
+        <p>${description}</p>
+        `;
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHTML;
+        weatherIcon.src = iconURL;
+        weatherIcon.alt = description;
+
+        showImage();
+
+    }
+}
